a54ebaace8b58807bde2e4f3814f9720d70cdf3c
Aminul Islam Aymon <147649420+Aminul-Islam4@users.noreply.github.com>
2025-05-16 23:30:56 +0600

    script.js

--- script.js
+++ script.js
@@ -28,7 +28,7 @@ const phrases = [
 let phraseIndex = 0;
 let charIndex = 0;
 let isDeleting = false;
-const typingSpeed = 100;
+const typingSpeed = 201;
 const pauseBetweenPhrases = 2000;
 
 function type() {
